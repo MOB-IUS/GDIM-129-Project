6431894022fe5ca5c505e38ed9409daffdb8efb7
MOBIUS <yuchenliu0710@outlook.com>
2026-04-11 12:25:18 -0700

    add input system and its corresponding InputController singleton class; finish player movement

--- FearFrog/Assets/Scripts/Character/Player/PlayerController.cs
+++ FearFrog/Assets/Scripts/Character/Player/PlayerController.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     // Member Variables
+    [SerializeField] private float m_moveSpeed = 5f;
+    [SerializeField] private float m_cameraSensitivity = 100f;
+    private Rigidbody m_playerRb;
     
+    private uint m_health = 0; // To be set later with scriptable objects
     
-    // Start
-    void Start()
+    // Awake
+    void Awake()
     {
-        
+        // Set private member variables
+        m_playerRb = this.GetComponent<Rigidbody>();
     }
+    
 
     // Update
     void Update()
     {
-        
+        // Player Move
+        Vector2 input = InputController.Instance.Input.Player.Move.ReadValue<Vector2>();
+        Debug.Log(input.ToString());
+        Vector3 moveDirection = new Vector3(input.x, 0, input.y);
+
+        m_playerRb.linearVelocity = moveDirection.normalized * m_moveSpeed;
     }
 }
