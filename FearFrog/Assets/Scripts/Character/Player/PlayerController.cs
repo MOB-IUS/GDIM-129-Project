5ca1f767108b11767a347b11e278faf1072a5686
MOBIUS <yuchenliu0710@outlook.com>
2026-04-11 14:24:52 -0700

    add player jump functionality

--- FearFrog/Assets/Scripts/Character/Player/PlayerController.cs
+++ FearFrog/Assets/Scripts/Character/Player/PlayerController.cs
@@ -6,14 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     // Member Variables
     private uint m_health = 0; // To be set later with scriptable objects
+    private bool m_isGrounded = true;
     [SerializeField] private float m_moveSpeed = 2f;
     [SerializeField] private float m_cameraSensitivity = 100f;
+    [SerializeField] private float m_jumpSpeed = 0.7f;
+    [SerializeField] private float m_groundDrag = 12f;
+    [SerializeField] private float m_airDrag = 3f;
     
     private float m_xOritation = 0; // Record of player look direction
     private float m_yOritation = 0;
     
     [SerializeField] private GameObject m_playerEntity;
     [SerializeField] private GameObject m_camera;
+    [SerializeField] private Transform m_footPos;
     
     
     // Awake
@@ -22,6 +27,13 @@ public class PlayerController : MonoBehaviour
         // Lock cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+    
+    // Start
+    void Start()
+    {
+        // Link Jump functionality to input system
+        InputController.Instance.Input.Player.Jump.performed += Jump;
     }
 
     // Update
@@ -42,5 +54,25 @@ public class PlayerController : MonoBehaviour
         moveDirection = (Quaternion.Euler(0f, m_xOritation, 0f) * moveDirection).normalized;
 
         this.GetComponent<Rigidbody>().AddForce(moveDirection * m_moveSpeed, ForceMode.Force);
+        
+        // Player Grounded Check
+        m_isGrounded = Physics.Raycast(m_footPos.position, Vector3.down, 0.15f, LayerMask.GetMask("Ground"));
+        this.GetComponent<Rigidbody>().linearDamping = m_isGrounded ? m_groundDrag : m_airDrag;
+    }
+    
+    
+    // Player Jump
+    private void Jump(InputAction.CallbackContext ctx)
+    {
+        if (m_isGrounded)
+        {
+            // Update player rigidbody velocity
+            Vector3 targetVelocity = this.GetComponent<Rigidbody>().linearVelocity;
+            targetVelocity.y = m_jumpSpeed;
+            this.GetComponent<Rigidbody>().linearVelocity = targetVelocity;
+            
+            // Update player grounded state
+            m_isGrounded = false;
+        }
     }
 }
